1f516c569f0f77a78e538561e9191b2360623c8a
Collin Kennedy <collinjkennedy@gmail.com>
2020-03-05 19:59:12 -0800

    last commit for tonight

--- donor_runner.py
+++ donor_runner.py
@@ -1,1 +1,14 @@
 # Your solution for the donor problem here
+from bst_tree import BST
+from bst_node import BSTNode
+
+
+if __name__ == "__main__":
+    firstBSTNode = BSTNode(10)
+    print(firstBSTNode.value)
+
+    firstBST = BST()
+    firstBST.add_value(10)
+    print(firstBST.root.value)
+
+
