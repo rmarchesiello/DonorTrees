f9941b8f07c285eae4e6daff4184a6e1b747dcbb
Collin Kennedy <collinjkennedy@gmail.com>
2020-03-07 12:17:53 -0800

    working on test cases

--- Trees/test/trees/test_bst.py
+++ Trees/test/trees/test_bst.py
@@ -12,18 +12,28 @@ class TestBST(unittest.TestCase):
     def test_create_tree(self):
         tree = BST()
         tree.add_value(100)
-        # tree.add_value(80)
-        # tree.add_value(200)
-        # tree.add_value(90)
-        # tree.add_value(70)
+        tree.add_value(80)
+        tree.add_value(200)
+        tree.add_value(90)
+        tree.add_value(70)
 
-        # root = BSTNode(100)
-        # root.left = BSTNode(80)
-        # root.right = BSTNode(200)
-        # root.left.left = BSTNode(70)
-        # root.left.right = BSTNode(90)
+        root = BSTNode(100)
+        root.leftChild = BSTNode(80)
+        root.rightChild = BSTNode(200)
+        root.leftChild.leftChild = BSTNode(70)
+        root.leftChild.rightChild = BSTNode(90)
+
+        # check if the list of their nodes' values are the same:
+        treeNodes = [tree.root.value, tree.root.leftChild.value, tree.root.leftChild.leftChild.value,
+                     tree.root.leftChild.rightChild.value,
+                     tree.root.rightChild.value]
+        rootNodes = [root.value, root.leftChild.value, root.leftChild.leftChild.value,
+                     root.leftChild.rightChild.value, root.rightChild.value]
+
+        self.assertEqual(treeNodes, rootNodes)
 
         cmp_tree = BST(root)
+        cmp_tree._num_nodes = 5
         self.assertEqual(tree, cmp_tree)
 
     def test_tree_not_eq(self):
