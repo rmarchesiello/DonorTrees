8bd0c4141cc0b2c17af056a0fb01970715cfb9c8
Collin Kennedy <collinjkennedy@gmail.com>
2020-03-09 18:36:33 -0700

    finished test_bst_node

--- Trees/test/nodes/test_bst_node.py
+++ Trees/test/nodes/test_bst_node.py
@@ -8,12 +8,42 @@ class TestBSTNode(unittest.TestCase):
         testNode = BSTNode(5)
         self.assertEqual(testNode.value, 5)
 
-    def testGetRightChild(self):
-        pass
+    def testGetRightChildWithNoChildren(self):
+        testNode = BSTNode(10)
+        self.assertEqual(testNode.getRightChild(), -1)
 
-    def testGetLeftChild(self):
-        pass
+    def testGetRightChildWithChildren(self):
+        testNode = BSTNode(15)
+        testNode.rightChild = 20
+        self.assertEqual(testNode.getRightChild(), 20)
 
+    def testGetLeftChildWithNoChildren(self):
+        testNode = BSTNode(20)
+        self.assertEqual(testNode.getLeftChild(), -1)
 
-if __name__ == '__main__':
+    def testGetLeftChildWithChildren(self):
+        testNode = BSTNode(50)
+        testNode.leftChild = 10
+        self.assertEqual(testNode.getLeftChild(), 10)
+
+    def testNumChildrenWithNoChildren(self):
+        testNode = BSTNode(15)
+        self.assertEqual(testNode.numChildren(), 0)
+
+    def testNumChildrenWithOneChild(self):
+        testNode = BSTNode(15)
+        testNode.leftChild = 10
+        testNode.children[0] = testNode.leftChild
+        self.assertEqual(testNode.numChildren(), 1)
+
+    def testNumChildrenWithTwoChildren(self):
+        testNode = BSTNode(15)
+        testNode.leftChild = 10
+        testNode.rightChild = 30
+
+        testNode.children[0] = testNode.leftChild
+        testNode.children[1] = testNode.rightChild
+        self.assertEqual(testNode.numChildren(), 2)
+
+if __name__ == "__main__":
     unittest.main()
